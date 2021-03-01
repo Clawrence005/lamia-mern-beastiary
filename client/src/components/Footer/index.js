d9507bc9113eed3c70233fd46bfc9e3e1e56b3ef
colin <clawrence005@yahoo.com>
2021-03-01 02:47:04 -0600

    basic styled footer w/ dummy info

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import './style.css';
+import logo from '../../assets/logo.svg';
 
 function Footer() {
 
   return (
     <footer>
-      <p>footer</p>
+      <img className="footer-img" src={logo} alt="beastiary logo" />
+      <p>beastiary 2021</p>
+      <hr />
+      <div className="footer-parent">
+        <div className="footer-child">
+          <p>Contact Us</p>
+          <ul>
+            <li><img className="footer-img" src={logo} alt="beastiary logo" /></li>
+            <li><p>beastiary 2021</p></li>
+          </ul>
+        </div>
+        <div className="footer-child">
+          <p>About</p>
+          <ul>
+            <li>instagram</li>
+            <li>dribbble</li>
+            <li>instagram</li>
+          </ul>
+        </div>
+        <div className="footer-child">
+          <p>Something else</p>
+          <ul>
+            <li>instagram</li>
+            <li>dribbble</li>
+            <li>instagram</li>
+          </ul>
+        </div>
+        <div className="footer-child">
+          <p>Contact Us</p>
+          <ul>
+            <li>instagram</li>
+            <li>dribbble</li>
+            <li>instagram</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
     </footer>
   )
 }
